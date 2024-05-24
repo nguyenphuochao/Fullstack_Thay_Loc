03d3ca8a026b6447e09f502dc518424a09238297
nguyenphuochao <nguyenphuochao123@gmail.com>
2024-05-24 21:26:11 +0700

    m

--- back-end/authentication/logout.php
+++ back-end/authentication/logout.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 session_start();
 // hủy tất cả các phần tử trong array $_SESSION
 session_destroy();
+// xóa lun cookie
+setcookie("email", null, time() - 3600);
 header("Location: login.html");
-?>
